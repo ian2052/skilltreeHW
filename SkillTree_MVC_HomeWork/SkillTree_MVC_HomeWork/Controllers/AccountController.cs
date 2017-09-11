ce8ba83768e9b977c93dab0fa65d1fbb0d234d83
Ian2052 <za2052@honghwa.com.tw>
2017-09-11 22:36:35 +0800

    Session1_HW

---
+++ SkillTree_MVC_HomeWork/SkillTree_MVC_HomeWork/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using SkillTree_MVC_HomeWork.Models.ViewModels;
+using static SkillTree_MVC_HomeWork.Models.ViewModels.AccountViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace SkillTree_MVC_HomeWork.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            Random rand = new Random();
+            var source = new AccountViewModel
+            {
+                // Type = "類別",
+                //Type = Enum.GetName(typeof(Types), rand.Next(2)),
+                Cost = 100,
+                Date = DateTime.Now,
+                Memo = "TEST"
+
+            };
+            int typeFlag = 0;
+            DateTime dTime = DateTime.Today;
+
+            var lst = new List<AccountViewModel>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                typeFlag = rand.Next(2);
+
+                var temp = new AccountViewModel
+                {
+                    Type = Enum.GetName(typeof(AccountViewModel.Types), typeFlag),
+                    Cost = rand.Next(500),
+                    Date = dTime.AddDays(rand.Next(-60,0)),
+                    Memo = typeFlag==0? Enum.GetName(typeof(AccountViewModel.incomeRemark), rand.Next(3)) : Enum.GetName(typeof(AccountViewModel.costRemark), rand.Next(4))
+                };
+                lst.Add(temp);
+            }
+
+            //source.dt.Add();
+
+            ViewData["lstAccount"] = lst;
+
+            return View();
+        }
+    }
+}
---
+++ SkillTree_MVC_HomeWork/SkillTree_MVC_HomeWork/Models/ViewModels/AccountViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SkillTree_MVC_HomeWork.Models.ViewModels
+{
+    public class AccountViewModel
+    {
+        public string Type { get; set; }
+
+        //[DisplayName("SDFSDFSDFSDF")]
+        public int Cost { get; set; }
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime Date { get; set; }
+        public string Memo { get; set; }
+
+        //public List<AccountViewModel> dt { get; set; }
+        public enum Types { 收入, 支出 }
+
+        public enum incomeRemark { 薪水, 發票中獎, 彩券中獎 }
+        public enum costRemark { 服裝, 飲食, 娛樂, 旅遊 }
+    }
+}
